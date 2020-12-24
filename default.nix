15f8b038c20734076d52efa410fd4cbaf02a4457
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-12-24 10:50:26 +0000

    add default.nix
    as a quick and reproducible way of supplying all
    development dependencies

---
+++ default.nix
@@ -0,0 +1,28 @@
+argsOuter@{...}:
+let
+  # specifying args defaults in this slightly non-standard way to allow us to include the default values in `args`
+  args = rec {
+    pkgs = import <nixpkgs> {};
+    go = pkgs.go;
+    localOverridesPath = ./local.nix;
+    forDev = true;
+  } // argsOuter;
+in (with args; {
+
+  terraformProviderCloudfoundryV3Env = (pkgs.stdenv.mkDerivation rec {
+    name = "tf-cfv3-env";
+    shortName = "tfcfv3";
+    buildInputs = with pkgs; [ go ]
+      ++ stdenv.lib.optionals forDev [ gitFull cacert cloudfoundry-cli ruby ];
+
+    LD_LIBRARY_PATH = "${pkgs.stdenv.lib.makeLibraryPath buildInputs}";
+    LANG="en_GB.UTF-8";
+    GOPATH = (toString (./.)) + "/.gopath";
+
+    shellHook = ''
+      export PS1="\[\e[0;36m\](nix-shell\[\e[0m\]:\[\e[0;36m\]${shortName})\[\e[0;32m\]\u@\h\[\e[0m\]:\[\e[0m\]\[\e[0;36m\]\w\[\e[0m\]\$ "
+
+      mkdir -p $GOPATH
+    '';
+  }).overrideAttrs (if builtins.pathExists localOverridesPath then (import localOverridesPath args) else (x: x));
+})
